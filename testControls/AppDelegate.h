203d989dc3024b9940bea6f38d70c19b31f5d6fc
AlexLiman <limanalex@mail.ru>
2016-05-02 20:53:17 +0300

    Initial Commit

---
+++ testControls/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testControls
+//
+//  Created by Alex on 02.05.16.
+//  Copyright © 2016 com.alexliman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testControls/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testControls
+//
+//  Created by Alex on 02.05.16.
+//  Copyright © 2016 com.alexliman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
